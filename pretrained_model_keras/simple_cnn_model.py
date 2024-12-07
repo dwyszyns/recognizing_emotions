fd34e8f3cd60523dadf8c813ebd00a1d74a00d3b
Dominika <01168920@pw.edu.pl>
2024-12-07 19:20:05 +0100

    added typing

--- pretrained_model_keras/simple_cnn_model.py
+++ pretrained_model_keras/simple_cnn_model.py
@@ -3,16 +3,17 @@ import matplotlib.pyplot as plt
 import cv2
 import dlib
 import glob
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.utils import to_categorical
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import LambdaCallback
 
 emotions = ["0", "1", "2", "3", "4", "5", "6", "7"]
 
-def create_simple_cnn_model(input_shape, num_classes, learning_rate):
+def create_simple_cnn_model(input_shape: tuple, num_classes: int, learning_rate: float):
     model = Sequential()
 
     model.add(Conv2D(6, kernel_size=(6, 6), activation='relu', input_shape=input_shape))
@@ -29,20 +30,26 @@ def create_simple_cnn_model(input_shape, num_classes, learning_rate):
     
     return model
 
-def train_model(X_train, y_train, model, batch_size=64, epochs=30):
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
-    return model
+def train_model(X_train: np.array, y_train: np.array, model:Sequential, batch_size: int =64, epochs: int =30):
+    print_lr_callback = LambdaCallback(on_epoch_begin=lambda epoch, logs: print(f"Learning Rate at epoch {epoch+1}: {model.optimizer.lr.numpy()}"))
+    history = model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2, callbacks=[print_lr_callback])
+    
+    train_losses = history.history['loss']
+    val_losses = history.history['val_loss']
+    
+    return model, train_losses, val_losses
 
-def test_model(X_test, y_test, model):
+
+def test_model(X_test: np.array, y_test: np.array, model: Sequential):
     y_pred = np.argmax(model.predict(X_test), axis=1)
     accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred)
     return y_pred, accuracy
 
-def extract_landmarks(image, predictor, detector):
+def extract_landmarks(image:np.array, predictor, detector):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
 
-    if len(rects) > 0:
+    if len(rects) > 0:  
         shape = predictor(gray, rects[0])
         coords = np.zeros((68, 2), dtype="int")
         for i in range(68):
@@ -52,7 +59,7 @@ def extract_landmarks(image, predictor, detector):
         return np.zeros((68 * 2,))
 
 
-def load_data_with_landmarks(emotion_list, base_dir="train", img_size=(48, 48)):
+def load_data_with_landmarks(emotion_list: list, base_dir: str ="train", img_size: tuple =(48, 48)):
     X = []
     y = []
     landmarks = []
@@ -67,8 +74,6 @@ def load_data_with_landmarks(emotion_list, base_dir="train", img_size=(48, 48)):
             image = cv2.imread(path)
             image_resized = cv2.resize(image, img_size) 
 
-            # image_resized = augment_image(image_resized)
-
             landmark_vector = extract_landmarks(image, predictor, detector)
             landmarks.append(landmark_vector)
 
@@ -82,11 +87,11 @@ def load_data_with_landmarks(emotion_list, base_dir="train", img_size=(48, 48)):
     
     return X, y, landmarks
 
-def change_image(landmarks, images):
+def change_image(landmarks: np.array, images: np.array):
     colored_images = []
     for i in range(len(images)):
-        jaw_points = landmarks[i][0:17]  # Punkty żuchwy
-        forehead_points = landmarks[i][17:27]  # Punkty górnej części twarzy
+        jaw_points = landmarks[i][0:17]
+        forehead_points = landmarks[i][17:27]
 
         points = np.concatenate((jaw_points, forehead_points[::-1]), axis=0)
 
@@ -116,21 +121,56 @@ if __name__ == "__main__":
     X_train, y_train, landmarks_train = load_data_with_landmarks(emotions, "train")
     X_test, y_test, landmarks_test = load_data_with_landmarks(emotions, "test")
     X_train = change_image(landmarks_train, X_train)
-    X_test = change_image(landmarks_test, X_test)
+    X_test = X_test.reshape(-1, 48, 48, 1)
     X_train = X_train / 255.0
     X_test = X_test / 255.0
     y_train = to_categorical(y_train, num_classes=len(emotions))
     y_test = to_categorical(y_test, num_classes=len(emotions))
 
     input_shape = X_train.shape[1:]
-    model = create_simple_cnn_model(input_shape, num_classes=len(emotions), learning_rate=0.001)
+    all_epoch_train_losses = []
+    all_epoch_test_losses = []
+    all_train_accuracies = []
+    all_test_accuracies = []
+    runs=6
+    for run in range(runs):
+        print(f"Training run {run + 1}/{runs}")
+        model = create_simple_cnn_model(input_shape, num_classes=len(emotions), learning_rate=0.001)
 
-    trained_model = train_model(X_train, y_train, model, epochs=20, batch_size=1)
+        trained_model, train_losses, val_losses = train_model(X_train, y_train, model, epochs=20, batch_size=1)
 
-    y_pred, accuracy = test_model(X_test, y_test, trained_model)
-    print("CNN Accuracy on test data:", accuracy)
+        all_epoch_train_losses.append(train_losses)
+        all_epoch_test_losses.append(val_losses)
+        
+        y_pred, test_accuracy = test_model(X_test, y_test, trained_model)
+        print("CNN Accuracy on test data:", test_accuracy)
+        y_pred, train_accuracy = test_model(X_train, y_train, trained_model)
+        print("CNN Accuracy on training data:", train_accuracy)
+        all_train_accuracies.append(train_accuracy)
+        all_test_accuracies.append(test_accuracy)
+
+        trained_model.save(f"cnn_model_{run}.h5")
     
-    y_pred, accuracy = test_model(X_train, y_train, trained_model)
-    print("CNN Accuracy on training data:", accuracy)
+    avg_epoch_train_losses = np.mean(all_epoch_train_losses, axis=0)
+    avg_epoch_test_losses = np.mean(all_epoch_test_losses, axis=0)
+    
+    with open("cnn_results.txt", "w") as f:
+        f.write("Average training loss per epoch across all runs:\n")
+        for epoch, loss in enumerate(avg_epoch_train_losses, 1):
+            f.write(f"Epoch {epoch}: {loss:.4f}\n")
+        
+        f.write("Average validating loss per epoch across all runs:\n")
+        for epoch, loss in enumerate(avg_epoch_test_losses, 1):
+            f.write(f"Epoch {epoch}: {loss:.4f}\n")
+        
+        f.write("\nTrain and Test Accuracy for each run:\n")
+        for run in range(runs):
+            f.write(f"Run {run + 1} - Train Accuracy: {all_train_accuracies[run]:.2f}%, "
+                    f"Test Accuracy: {all_test_accuracies[run]:.2f}%\n")
 
-    trained_model.save('simple_cnn_emotion_model.h5')
+        avg_train_accuracy = np.mean(all_train_accuracies)
+        avg_test_accuracy = np.mean(all_test_accuracies)
+        f.write("\nAverage Train Accuracy: {:.2f}%\n".format(avg_train_accuracy))
+        f.write("Average Test Accuracy: {:.2f}%\n".format(avg_test_accuracy))
+
+    print("Training complete. Results saved to training_results.txt")
